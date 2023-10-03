ee2e86b8b9f84d6d104095370740fa4e17f40c12
swastik801 <146020181+swastik801@users.noreply.github.com>
2023-10-03 17:36:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ m = leafmap.Map(center=[40, -100], zoom=4, tiles="stamentoner")
 
 # Add a heatmap to the map
 m.add_heatmap(
-    df,
+    df=df,
     latitude="Latitude",
     longitude="Longitude",
     value="Pincode",
