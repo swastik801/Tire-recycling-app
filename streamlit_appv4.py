48ec32315d60b7127f0752c3dd364c14a9584a8e
swastik801 <146020181+swastik801@users.noreply.github.com>
2023-10-07 12:33:34 +0530

    Update streamlit_appv4.py

--- streamlit_appv4.py
+++ streamlit_appv4.py
@@ -11,10 +11,10 @@ df = pd.read_csv("Tire_v1.csv", sep=',')
 
 # Check if the DataFrame is loaded correctly
 if "Latitude" in df.columns and "Longitude" in df.columns:
-  st.write("DataFrame loaded successfully.")
+    st.write("DataFrame loaded successfully.")
 else:
-  st.write("Error: Latitude and Longitude columns not found in the DataFrame.")
-  st.stop() # Stop the Streamlit app if there's an issue with the dataset
+    st.write("Error: Latitude and Longitude columns not found in the DataFrame.")
+    st.stop()  # Stop the Streamlit app if there's an issue with the dataset
 
 # Convert Tread_Depth to float and replace NaN values with 0
 df['Tread_Depth'] = pd.to_numeric(df['Tread_Depths'], errors='coerce')
@@ -28,7 +28,7 @@ m = folium.Map(location=[40, -100], zoom_start=4)
 
 # Prepare data for Random Forest model
 X = df[['Tread_Depth', 'Odometer_Reading', 'Age']]
-y = df['Condition'] # Assuming 'Condition' is your target variable
+y = df['Condition']  # Assuming 'Condition' is your target variable
 
 # Train Random Forest model
 model = RandomForestClassifier(random_state=42)
@@ -36,40 +36,40 @@ model.fit(X, y)
 
 # Streamlit app
 def main():
-  st.title("Tyre Recycling Sustainability")
+    st.title("Tyre Recycling Sustainability")
 
-  # Display the image
-  st.image("Tire_image.jpeg", use_column_width=True)
+    # Display the image
+    st.image("Tire_image.jpeg", use_column_width=True)
 
-  # Create a sidebar
-  st.sidebar.header("Options")
+    # Create a sidebar
+    st.sidebar.header("Options")
 
-  # Add a button for prediction at the top of sidebar
-  if st.sidebar.button("Predict"):
-    # Use trained model to make prediction
-    odometer_reading = st.sidebar.selectbox(
-      "Select Odometer Reading",
-      df["Odometer_Reading"].unique()
-    )
-     
-    age = st.sidebar.selectbox(
-      "Select Age",
-      df["Age"].unique()
-    )
-     
-    prediction = model.predict([[0, odometer_reading, age]])[0]
-    st.write(f"AI algorithm predicts that condition of Tire is {prediction}")
+    # Add a button for prediction at the top of sidebar
+    if st.sidebar.button("Predict"):
+        # Use trained model to make prediction
+        odometer_reading = st.sidebar.selectbox(
+            "Select Odometer Reading",
+            df["Odometer_Reading"].unique()
+        )
+         
+        age = st.sidebar.selectbox(
+            "Select Age",
+            df["Age"].unique()
+        )
+         
+        prediction = model.predict([[0, odometer_reading, age]])[0]
+        st.write(f"AI algorithm predicts that condition of Tire is {prediction}")
 
-  # Add a button for the heatmap
-  if st.sidebar.button("Show Heatmap"):
-    # Create a new DataFrame for heatmap data
-    heatmap_data = df.groupby(['Latitude', 'Longitude', 'Tire_Brand']).size().reset_index(name='counts')
+    # Add a button for the heatmap
+    if st.sidebar.button("Show Heatmap"):
+        # Create a new DataFrame for heatmap data
+        heatmap_data = df.groupby(['Latitude', 'Longitude', 'Tire_Brand']).size().reset_index(name='counts')
 
-    # Add a heatmap layer to the map
-    HeatMap(data=heatmap_data[['Latitude', 'Longitude', 'counts']].values.tolist(), radius=15).add_to(m)
+        # Add a heatmap layer to the map
+        HeatMap(data=heatmap_data[['Latitude', 'Longitude', 'counts']].values.tolist(), radius=15).add_to(m)
 
-    # Display the map with the heatmap
-    folium_static(m)
+        # Display the map with the heatmap
+        folium_static(m)
 
 if __name__ == "__main__":
-  main()
+    main()
