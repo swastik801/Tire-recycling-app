ae9fe6b007a8256de3c0a332a576850c1ecb2a2f
swastik801 <146020181+swastik801@users.noreply.github.com>
2023-10-08 15:03:17 +0530

    Update streamlit_appv6.py

--- streamlit_appv6.py
+++ streamlit_appv6.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 
 # Load the data
-df = pd.read_csv("Tire_2.csv", sep=',')
+df = pd.read_csv("Tire_3.csv", sep=',')
 
 # Check if the DataFrame is loaded correctly
 if "Latitude" in df.columns and "Longitude" in df.columns:
